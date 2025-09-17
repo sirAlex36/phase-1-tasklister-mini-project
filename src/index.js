3d09dc1d68a97489930fe9a3ddf3fce7a0ca6d94
SirAlex36 <alexngenoo@gmail.com>
2025-09-17 11:55:29 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const addElement=document.querySelector('#create-task-form')
+const tasklist=document.querySelector('#tasks')
+
+addElement.addEventListener("submit",function(event){
+event.preventDefault() 
+  
+const imput=document.querySelector("#new-task-description")
+const taskstodo=imput.value.trim()
+
+if(taskstodo !==""){
+  const ul=document.createElement("ul")
+  ul.textContent = taskstodo;
+  tasklist.appendChild(ul);
+
+  const deleteBtn=document.createElement('button')
+  deleteBtn.textContent='X'
+  deleteBtn.style.marginLeft="10px"
+
+  deleteBtn.addEventListener("click" ,() => {
+    ul.remove() })
+ 
+  ul.appendChild(deleteBtn)
+  tasklist.appendChild(ul)
+
+  imput.value = "";
+  }
+  
+ 
+})
+
 });
+
